13623c0a26d83b5601a2b95a5dc9341ef513b0b4
alexarmasu4 <135939943+alexarmasu4@users.noreply.github.com>
2023-09-26 13:02:35 -0700

    api url switch

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -34,6 +34,8 @@ const Home = ({ token }) => {
       {
         headers: {
           'content-type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
         },
       }
     );
